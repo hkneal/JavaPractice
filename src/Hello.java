372c3ae879cb2f42854442a3665c2c5f0fd6fa75
hkneal <hiramk@comcast.net>
2018-03-28 12:27:29 -0700

    Initial Push

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World, & Hiram!");
+
+        int myfirstNumber = 7;
+        System.out.println(myfirstNumber);
+
+        byte myByte = 7;
+        short myShort = 22978;
+        int myInt = 45;
+        long myLong = 50000L + 10L * (myByte + myShort + myInt);
+
+        System.out.println(myLong);
+    }
+}
